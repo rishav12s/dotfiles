cf81904480fd49d87daa27125a5511bce76f9f9e
rishav12s <eternal12rinkira06@gmail.com>
2025-02-04 18:56:06 +0530

    update the dots

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,6 +1,18 @@
 function fish_greeting
-    if type -q colorscript
-        colorscript random
-        #pfetch
+    set term (string lower -- $TERM)
+
+    switch $term
+        case '*kitty*' #kitty
+            colorscript random
+        case 'foot' #foot
+            fastfetch -c ~/.config/fastfetch/minimal_foot.jsonc
+        case '*xterm-256*' #foot with xterm-256
+            fastfetch -c ~/.config/fastfetch/minimal_foot.jsonc
+        case '*screen-256*' #tmux
+            fastfetch -c ~/.config/fastfetch/arch.jsonc
+        case '*ghostty*' #ghostty
+            colorscript random
+        case '*' #any other term
+            fastfetch -c ~/.config/fastfetch/arch.jsonc  # Default behavior
     end
 end
